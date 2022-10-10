442da7e61ba65600428cfcc00426ce9716b5ed71
Beatrizrochelly10@gmail.com <beatrizrochelly10@gmail.com>
2022-10-09 21:43:18 -0300

    configurações inicias do projeto

---
+++ brendarochellyart/src/main/java/com/brendarochellyart/brendarochellyart/BrendarochellyartApplication.java
@@ -0,0 +1,13 @@
+package com.brendarochellyart.brendarochellyart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrendarochellyartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrendarochellyartApplication.class, args);
+	}
+
+}
---
+++ brendarochellyart/src/test/java/com/brendarochellyart/brendarochellyart/BrendarochellyartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brendarochellyart.brendarochellyart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrendarochellyartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
